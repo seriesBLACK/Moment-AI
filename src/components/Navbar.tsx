c7ccde8eb1a6e82d09f6aa8f1f5db54a1e296d0c
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-11-09 00:24:37 +0300

    added Navbar & Next auth with Kinde

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,52 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+
 
 const Navbar = () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blure-lg transition-all">
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
-          <Link href='/' className="flex z-40 font-semibold">
-            Moment.
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link
+            href='/'
+            className='flex z-40 font-semibold'>
+            <span>Moment.</span>
           </Link>
+
+          <div className='hidden items-center space-x-4 sm:flex'>
+            <>
+              <Link href='pricing' className={buttonVariants({
+                variant: 'ghost',
+                size: 'sm',
+              })}>
+
+                Pricing
+              </Link>
+              <LoginLink className={buttonVariants({
+                variant: 'ghost',
+                size: 'sm',
+              })}>
+                Sign in
+              </LoginLink>
+              <RegisterLink className={buttonVariants({
+
+                size: 'sm',
+              })}>
+                Get started
+              </RegisterLink>
+            </>
+          </div>
+
+
         </div>
       </MaxWidthWrapper>
     </nav>
